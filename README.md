9550e4fb255a496ec7cdfd1c04340c2f1c85299c
Gerrit Riessen <gerrit.riessen@gmail.com>
2011-05-28 15:09:14 +0200

    Slight modification in the readme and allow for sqlite usage

--- README.md
+++ README.md
@@ -5,13 +5,22 @@ Github High Scores is a fun way to rank Github repository contributors in a 8-bi
 ## Installation
 
     git clone git://github.com/leereilly/github-high-scores.git
+    rvm install ruby-1.8.7-p334 ## didn't have correct version
     cd github-high-scores
-    ruby app.rb
+    gem install bundler
+    bundle
+    db_use=sqlite_default ruby app.rb
 
 ## Configuration
 
-You'll need to set environment variables on your box. Locally, you can set them in your .bash_profile...
+You can either use mysql, sqlite or sqlite with default setting
 
+For mysql:
+
+You'll need to set environment variables on your box. Locally, you can
+set them in your .bash_profile...
+
+    export db_use=mysql
     export db_user=some_username
     export db_pass=some_password
     export db_host=some_host
@@ -19,7 +28,20 @@ You'll need to set environment variables on your box. Locally, you can set them 
 
 On Heroku, you can do something like this...
 
-    heroku config:add db_user=XXX db_pass=XXX db_host=XXX db_data=XXX
+    heroku config:add db_user=XXX db_pass=XXX db_host=XXX db_data=XXX db_use=mysql
+
+For sqlite:
+
+    export db_use=sqlite
+    export db_path=/some/absolute/path/to/my.db
+
+For sqlite + default:
+
+    export db_use=sqlite_default
+
+This then creates a my.db database file in db/
+
+See app.rb for details.
 
 ## Contribute
 
--- lib/Contributor.rb
+++ lib/Contributor.rb
@@ -4,41 +4,37 @@ require 'net/http'
 require 'json'
 require 'uri'
 
-DataMapper::Logger.new($stdout, :debug)
-puts "mysql://#{ENV['db_user']}:#{ENV['db_pass']}@#{ENV['db_host']}/#{ENV['db_data']}"
-DataMapper.setup(:default, "mysql://#{ENV['db_user']}:#{ENV['db_pass']}@#{ENV['db_host']}/#{ENV['db_data']}")
-
 class Contributor
   include DataMapper::Resource
-  
+
   API_VERSION = 'v2'
   BASE_URL = 'http://github.com/api/' + API_VERSION + '/json/user/show/'
-  
+
   property :id, Serial
   property :login, String
   property :gravatar_id, String
   property :contributions, String
-  
+
   def self.create_from_user_and_repo(user, repo)
     stored_user = User::create_from_username(user)
     stored_repo = Repo::create_from_username_and_repo(user, repo)
-    
+
     contributors_url = "http://github.com/api/v2/json/repos/show/#{user}/#{repo}/contributors"
     contributors_feed = Net::HTTP.get_response(URI.parse(contributors_url))
     contributors = contributors_feed.body
     contributors_result = JSON.parse(contributors)
     repository_contributors =  contributors_result['contributors']
     contributors_array = Array.new
-   
+
     repository_contributors.each do |repository_contributor|
       contributor = Contributor.new
       contributor.login = repository_contributor['login']
       contributor.gravatar_id = repository_contributor['gravatar_id']
       contributor.contributions = repository_contributor['contributions']
       contributor.save
-    end    
+    end
   end
-  
+
   def self.get_json_response(url)
     Net::HTTP.get_response(URI.parse(url))
   end
