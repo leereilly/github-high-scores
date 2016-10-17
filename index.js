329dc8a4d18f1b55410940d7f2a419aad3c6bc47
Jack Lewin <jl34g14@soton.ac.uk>
2016-10-17 15:10:59 +0100

    Take URL, redirect, display scores (or error page)
    Using validator to check for a valid URL, and the github node package to
    query the GitHub API.

--- index.js
+++ index.js
@@ -1,18 +1,112 @@
-var express = require('express');
-var app = express();
+var express   = require('express'),
+    validator = require('validator'),
+    githubapi = require('github'),
+    github    = new githubapi(),
+    app       = express();
 
 app.set('port', (process.env.PORT || 5000));
 
+// public directory is used for assets
 app.use(express.static(__dirname + '/public'));
 
-// views is directory for all template files
+// views directory is used for template files
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
-
-app.get('/', function(request, response) {
-  response.render('pages/index');
-});
 
 app.listen(app.get('port'), function() {
   console.log('Running on port', app.get('port'));
 });
+
+
+app.get('/', function(req, res) {
+  // check whether the repo GET parameter is a valid URL
+  var url      = typeof(req.query.repo) === 'undefined' ? '' : req.query.repo,
+      isURL    = validator.isURL(url, {
+        protocols: ['http','https'],
+        allow_underscores: true
+      });
+
+  if(isURL) {
+    // if it's a valid url, redirect to /user/repo
+    var user = get_user_from_github_url(url),
+        repo = get_repo_from_github_url(url);
+
+    res.redirect(user + '/' + repo);
+    return;
+
+  } else if(url !== '') {
+    // if the user entered an invalid url, render the home page with an error
+    res.render('pages/notfound', { title: '404', error: 'Sorry, but this cat is in another castle!' });
+  }
+
+  // if the user hasn't entered a url, just render the home page
+  res.render('pages/index');
+});
+
+app.get('/:user/:repo/?', function(req, res){
+  // get the results
+  var scores = get_high_scores(req.params.user, req.params.repo)
+  .then(function(results) {
+
+    // render the results
+    res.render('pages/score', { scores: results });
+
+  }, function(err) {
+    
+    // handle error
+    console.log(err);
+    res.render('pages/notfound', { title: '404', error: 'Sorry, but this cat is in another castle!' });
+  
+  });
+});
+
+
+function get_user_from_github_url(sanitized_github_url) {
+  return sanitized_github_url.split('/')[3];
+};
+
+function get_repo_from_github_url(sanitized_github_url) {
+  return sanitized_github_url.split('/')[4];
+};
+
+function get_high_scores(user, repo) {
+  return new Promise(function(resolve, reject) {
+
+    github.repos.getContributors({
+      owner: user,
+      repo: repo,
+      anon: true
+    }, function(err, res) {
+
+      // if there's an error, reject the promise
+      if(err) {
+        reject(err);
+        return;
+      } else if(typeof(res) == 'undefined') {
+        reject('Repository not found');
+        return;
+      }
+
+      // empty array to contain results
+      var results = [];
+
+      // score = contributions * 10
+      // iterate through each user
+      Promise.all( res.map( function(user) {
+
+        // add current user to the results array
+        return {
+          username : user.login,
+          score    : user.contributions * 10
+        };
+        
+      })).then( function(results) {
+
+        // once the users have been processed, return the results
+        resolve(results);
+      });
+
+    });
+
+  });
+};
